8cb065669675121b4c9977d2fc6780536993b87e
NATHALIA VENTURA KAWAKAMI <23111822@CFP127ALUNO.LOCAL>
2023-11-29 12:20:56 -0300

    😓

---
+++ module/funcoes.js
@@ -0,0 +1,135 @@
+var infos = require('./pizzariaInfos.JS')
+
+var categoria = infos.categorias
+var produtos = infos.produtosJSON
+
+
+const mostrarPizza = (idProduto) => {
+    const arrayPizza = produtos.pizzas
+    const arrayComentarios = arrayPizza[0].comentarios
+
+    let arrayC = []
+    let arrayCo = []
+    let status = false
+   
+    arrayPizza.forEach(function(produto){
+        if(produto.id == idProduto){
+            let jsonPizza = {}
+
+            jsonPizza.foto = produto.foto
+            jsonPizza.nome = produto.nome
+            jsonPizza.favorito = produto.favorito
+            jsonPizza.valor = produto.preco
+            jsonPizza.descricao = produto.descricao
+            jsonPizza.avl = produto.avaliacao
+            
+            arrayComentarios.forEach(function(comen){
+                const fotoUsuario = comen.usuario.foto_usuario
+                const nomeUsuario = comen.usuario.nome
+                let jsonC = {}
+    
+               jsonC.fotoU = fotoUsuario
+               jsonC.nomeU = nomeUsuario
+               jsonC.data = comen.data
+               jsonC.avlComentario = comen.avl_comen
+               jsonC.titulo = comen.titulo
+               jsonC.comentario = comen.coment_inteiro
+    
+               arrayCo.push(jsonC)
+            })
+    
+            jsonPizza.comentario = arrayCo
+            
+            arrayC.push(jsonPizza)
+    
+            status = true
+        }
+        })
+    
+        let jsonT = {arrayC}
+        if(status)
+        return jsonT
+        else
+        return false   
+}
+
+const getFavoritos = () => {
+    const arrayPizza = produtos.pizzas
+    let arrayC = []
+    let status = false
+    arrayPizza.forEach(function(produto){
+        if(produto.favorito == true){
+            let jsonPizza = {}
+            jsonPizza.foto = produto.foto
+            jsonPizza.nome = produto.nome
+            jsonPizza.valor = produto.preco
+            arrayC.push(jsonPizza)
+            status = true
+        }
+        })
+        let jsonT = {arrayC}
+        if(status)
+        return jsonT
+        else
+        return false   
+}
+
+const mostrarBebida = (idProduto) => {
+    const arrayBebida = produtos.bebidas
+
+    let arrayC = []
+    let status = false
+   
+    arrayBebida.forEach(function(produto){
+        if(produto.id == idProduto){
+            let jsonPizza = {}
+
+            jsonPizza.foto = produto.foto
+            jsonPizza.nome = produto.nome
+            jsonPizza.favorito = produto.favorito
+            jsonPizza.valor = produto.preco
+            jsonPizza.descricao = produto.descricao
+            jsonPizza.avl = produto.avaliacao
+                        
+            arrayC.push(jsonPizza)
+    
+            status = true
+        }
+        })
+    
+        let jsonT = {arrayC}
+        if(status)
+        return jsonT
+        else
+        return false   
+}
+
+const mostrarBebidas = (idProdutos) => {
+    const arrayBebida = produtos.bebidas
+
+    let arrayC = []
+    let status = false
+   
+    arrayBebida.forEach(function(produto){
+            let jsonPizza = {}
+
+            jsonPizza.foto = produto.foto
+            jsonPizza.nome = produto.nome
+            jsonPizza.favorito = produto.favorito
+            jsonPizza.valor = produto.preco
+            jsonPizza.descricao = produto.descricao
+            jsonPizza.avl = produto.avaliacao
+                        
+            arrayC.push(jsonPizza)
+    
+            status = true
+        })
+    
+        let jsonT = {arrayC}
+        if(status)
+        return jsonT
+        else
+        return false   
+}
+
+//console.log(mostrarPizza(3))
